d91b42e5ae8c87372c1faa7b54c40ef2d1c33916
Alexander Larin <doremilarin@gmail.com>
2025-06-21 16:55:08 +0300

    feat: add ARIA attributes to radio buttons

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,7 +2,9 @@ import { toggleActiveClass } from "./scripts/radio-buttons.js";
 
 function main() {
 	const filters = document.querySelector('[data-component="filters"]');
-	toggleActiveClass(filters);
+	if (filters.childElementCount > 0) {
+		toggleActiveClass(filters);
+    }
 }
 
 document.addEventListener("DOMContentLoaded", () => {
--- assets/js/scripts/radio-buttons.js
+++ assets/js/scripts/radio-buttons.js
@@ -13,10 +13,19 @@ export function toggleActiveClass(component) {
 			}
 
 			[...children].forEach((el) => {
+				const radioBtn = el.querySelector('input[type="radio"]');
+
+				radioBtn.removeAttribute("checked");
 				el.classList.remove(activeClass);
+				el.setAttribute("aria-checked", "false");
 			});
 
+            const parent = e.target.parentElement;
+			const radioBtn = parent.querySelector('input[type="radio"]');
+
+			radioBtn.setAttribute("checked", "");
 			e.target.parentElement.classList.add(activeClass);
+			e.target.parentElement.setAttribute("aria-checked", "true");
 
 			e.preventDefault();
 		});
